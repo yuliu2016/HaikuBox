039d1ea0de8bd948769c2941cc4cbe3b9b660e63
jgsteplujr <juice@yus-imac.local>
2015-12-28 17:39:46 -0500

    Initial Commit

---
+++ HaikuBox/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HaikuBox
+//
+//  Created by Yu Liu on 2015-12-28.
+//
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
